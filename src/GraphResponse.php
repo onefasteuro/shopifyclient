d4f08da269e1a0443768b190fac70f11d8188317
Oliver <onefasteuro@gmail.com>
2019-10-10 16:53:31 -0400

    Edits

--- src/GraphResponse.php
+++ src/GraphResponse.php
@@ -1,109 +1,112 @@
 <?php
-
-namespace onefasteuro\ShopifyClient;
-
-use onefasteuro\ShopifyClient\Exceptions\ErrorsFoundException;
-use onefasteuro\ShopifyClient\Exceptions\NotAuthorizedException;
-use onefasteuro\ShopifyClient\Exceptions\NotFoundException;
-use Requests_Response;
-
-class GraphResponse implements GraphResponseInterface
-{
-	protected $raw;
-	protected $parsed_response;
 	
-	public function __construct(Requests_Response $response)
+	namespace onefasteuro\ShopifyClient;
+	
+	use onefasteuro\ShopifyClient\Exceptions\ErrorsFoundException;
+	use onefasteuro\ShopifyClient\Exceptions\NotAuthorizedException;
+	use onefasteuro\ShopifyClient\Exceptions\NotFoundException;
+	use Requests_Response;
+	
+	class GraphResponse implements GraphResponseInterface
 	{
-		$this->raw = $response;
-		$this->parsed_response = $this->parse($this->raw);
-	}
-	
-	
-	public function assertSuccessResponse()
-	{
-		if($this->isNotFound()) {
-			throw new NotFoundException('Could not locate this url.', $this->status_code);
+		protected $raw;
+		protected $parsed_response;
+		
+		public function __construct(Requests_Response $response)
+		{
+			$this->raw = $response;
+			$this->parsed_response = $this->parse($this->raw);
 		}
 		
-		if($this->isNotAuthorized()){
-			throw new NotAuthorizedException('This request is not authorized', $this->status_code);
-		}
 		
-		if($this->hasErrors()) {
-			$errors = $this->assertErrors();
-			throw new ErrorsFoundException('Your request contained some errors: ' . $errors, 400);
-		}
-		
-		return $this;
-	}
-	
-	public function raw()
-	{
-		return $this->raw;
-	}
-	
-	public function assertErrors()
-	{
-		$output = [];
-
-		
-		if(count($this->parsed_response['errors']) === 1) {
-			$output[] = $this->parsed('errors');
-		}
-		
-		if (count($this->parsed('errors')) > 1) {
-			foreach($this->parsed_response['errors'] as $msg) {
-				$output[] = $msg['message'];
+		public function assertSuccessResponse()
+		{
+			if($this->isNotFound()) {
+				throw new NotFoundException('Could not locate this url.', $this->status_code);
 			}
+			
+			if($this->isNotAuthorized()){
+				throw new NotAuthorizedException('This request is not authorized', $this->status_code);
+			}
+			
+			if($this->hasErrors()) {
+				$errors = $this->assertErrors();
+				throw new ErrorsFoundException('Your request contained some errors: ' . $errors, 400);
+			}
+			
+			return $this;
 		}
 		
+		public function raw()
+		{
+			return $this->raw;
+		}
 		
-		return implode("\n", $output);
+		public function assertErrors()
+		{
+			$output = [];
+			
+			
+			$errors = $this->parsed('errors');
+			
+			if(is_array($errors)) {
+				if(count($errors) === 1) {
+					$output[] =$errors[0]['message'];
+				}
+				
+				if (count($errors) > 1) {
+					foreach($errors as $msg) {
+						$output[] = $msg['message'];
+					}
+				}
+			}
+			
+			return implode("\n", $output);
+		}
+		
+		public function hasErrors()
+		{
+			return (array_key_exists('errors', $this->parsed())) ? true : false;
+		}
+		
+		public function __get($k)
+		{
+			return $this->raw->$k;
+		}
+		
+		public function __call($method, $params)
+		{
+			return call_user_func_array([$this->raw, $method], $params);
+		}
+		
+		public function parsed($key = null)
+		{
+			return ($key === null) ? $this->parsed_response : $this->parsed_response[$key];
+		}
+		
+		public function data()
+		{
+			return $this->parsed('data');
+		}
+		
+		public static function parse($raw)
+		{
+			return json_decode($raw->body, true);
+		}
+		
+		public function isNotFound()
+		{
+			return $this->raw->status_code === 404 ? true : false;
+		}
+		
+		public function isNotAuthorized()
+		{
+			return $this->raw->status_code === 401 ? true : false;
+		}
+		
+		public function isOk()
+		{
+			return $this->raw->status_code === 200 ? true : false;
+		}
+		
 	}
-	
-	public function hasErrors()
-	{
-		return (array_key_exists('errors', $this->parsed())) ? true : false;
-	}
-	
-	public function __get($k)
-	{
-		return $this->raw->$k;
-	}
-	
-	public function __call($method, $params)
-	{
-		return call_user_func_array([$this->raw, $method], $params);
-	}
-	
-	public function parsed($key = null)
-	{
-		return ($key === null) ? $this->parsed_response : $this->parsed_response[$key];
-	}
-	
-	public function data()
-	{
-		return $this->parsed('data');
-	}
-	
-	public static function parse($raw)
-	{
-		return json_decode($raw->body, true);
-	}
-	
-	public function isNotFound()
-	{
-		return $this->raw->status_code === 404 ? true : false;
-	}
-	
-	public function isNotAuthorized()
-	{
-		return $this->raw->status_code === 401 ? true : false;
-	}
-	
-	public function isOk()
-	{
-		return $this->raw->status_code === 200 ? true : false;
-	}
-	
-}
